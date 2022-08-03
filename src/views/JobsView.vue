10566e65a9cbcc789cf71776fcaf4bb679cee073
Becky Newton <iambexnewton@gmail.com>
2022-08-03 13:01:19 +0200

    pages, routing added

---
+++ src/views/JobsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [{ title: "dfghm", id: 1, details: "asdfghjmnbvcxzvbnm" }],
+    };
+  },
+};
+</script>

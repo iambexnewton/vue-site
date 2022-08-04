6537a84698f9d70d914a562b2acfabde838b2169
Becky Newton <iambexnewton@gmail.com>
2022-08-04 13:04:47 +0200

    custom error on submit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,44 +1,67 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>Lorem</p>
+    <h1>This is rate your set page</h1>
+    <p>Lorem blah blah </p>
   </div>
-<form>
+
+<form @submit.prevent="handleSubmit">
     <label>Date</label>
-    <input type="date" required  v-model='date' />
+    <input type="date" v-model='date' />
+    <!-- <div class="error" v-if="dateErrorMessage">{{dateErrorMessage}}</div> -->
 
     <label>Set</label>
-    <input type="set" required  v-model='set' />
+    <input type="sets" v-model='sets' />
+    <div class="error" v-if="setsErrorMessage">{{setsErrorMessage}}</div>
 
-    
-   <div class="score">
+  <div class="score">
+    <h2>Score</h2>
+    <ul>
+<li>
+  <label>1</label>
+<input type="checkbox" value="one" v-model="scores">
 
-<input type="checkbox">
-<label>1</label> 
+</li>
 
-   <input type="checkbox">
-   <label>2</label>
+<li>
+  <label>2</label>
+   <input type="checkbox" value="two" v-model="scores">
+   
+</li>
 
-   <input type="checkbox">
-   <label>3</label>
+<li> <label>3</label>
+   <input type="checkbox" value="three" v-model="scores">
 
-   <input type="checkbox">
-   <label>4</label>
+</li>
+<li>
+  <label>4</label>
+   <input type="checkbox" value="four" v-model="scores"></li>
+   
+<li>
+<label>5</label>
+   <input type="checkbox" value="five" v-model="scores">
+   </li>
+   </ul>
+    </div>  
 
-   <input type="checkbox">
-   <label>5</label>
-    </div>
-  
 
     <label>Notes</label>
-    <input type="notes" required  v-model='notes' />
+    <input type="text" required  v-model='notes' />
+<div class="submit">
+    <button>Submit</button>
+  </div>
+ </form>
+    
 
-  </form>
+ 
+
+  
 
   <p>Date : {{date}}</p>
-  <p>Set : {{set}}</p>
-  <p>Score : {{score}}</p>
+  <p>Set : {{sets}}</p>
+  <p>Score : {{scores}}</p>
   <p>Notes : {{notes}}</p>
+
+
 </template>
 
 
@@ -48,10 +71,17 @@
 export default {
   data() {
     return {
-      date: '', 
-      set: '',
-      score: '',
+      date: [], 
+      sets: [],
       notes: '',
+      scores: [],
+      data: [],
+      setsErrorMessage: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+     this.errorMessage = this.sets.length > 0 ? '' : 'Please enter the set details'  
     }
   }
 }
@@ -67,7 +97,7 @@ form {
   padding: 40px;
   border-radius: 10px;
 }
-label {
+label, h2 {
   color: #aaa;
   display: inline-block;
   margin: 25px 0px 5px;
@@ -85,12 +115,42 @@ input {
   border-bottom: 1px solid #ddd;
   color: #555;
 }
+.score {
+   display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd
+}
+button {
+  background: blue;
+  border: 0;
+  padding: 12px 16px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
 
-input[type="checkbox"] {
+input[type="checkbox"]
+ {
   display: inline-block;
   width: 16px;
-  margin:0 10px 0 0;
+  margin:0 30px 0 0;
   position: relative;
   top: 2px;
+  
 }
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+
+
 </style>

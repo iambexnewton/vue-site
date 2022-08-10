46f03d51198512cf6558b2d8cc155d97c7f4cc3c
Becky Newton <iambexnewton@gmail.com>
2022-08-10 07:09:35 +0200

    extracted the form component and added it into the correct view, functional

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,174 @@
+<template>
+
+
+<button @click="toggleShowForm">UPDATE YOUR SET LISTSX</button>
+
+  <section v-if="showForm">
+<form @submit.prevent="handleSubmit">
+    <label>Date</label>
+    <input type="date" v-model='date' />
+    <!-- <div class="error" v-if="dateErrorMessage">{{dateErrorMessage}}</div> -->
+
+    <label>Name</label>
+    <input type="name" v-model='name' />
+    <!-- <div class="error" v-if="setsErrorMessage">{{setsErrorMessage}}</div> -->
+
+  <div class="score">
+    <h2>Score</h2>
+    <ul>
+<li>
+  <label>1</label>
+<input type="checkbox" value="one" v-model="scores">
+</li>
+<li>
+  <label>2</label>
+   <input type="checkbox" value="two" v-model="scores">
+</li>
+<li> <label>3</label>
+   <input type="checkbox" value="three" v-model="scores">
+</li>
+<li>
+  <label>4</label>
+   <input type="checkbox" value="four" v-model="scores"></li>
+<li>
+<label>5</label>
+   <input type="checkbox" value="five" v-model="scores">
+   </li>
+   </ul>
+    </div>  
+
+
+    <label>Notes</label>
+    <input type="text" v-model='notes' />
+
+
+ <label>Photo URL</label>
+    <input type="text"  v-model='photo' />
+
+<div class="submit">
+    <button>Submit</button>
+  </div>
+ </form>
+ </section>
+
+</template>
+
+
+<script>
+
+export default {
+  name: "FormComponent",
+    data: function () {
+        return {
+            showForm: false,
+            date: [],
+            notes: "",
+            name: "",
+            scores: [],
+            photo: "",
+            data: [],
+            setsErrorMessage: "",
+            loading: false,
+            type: [],
+        };
+    },
+    methods:{
+        toggleShowForm() {
+            this.showForm = !this.showForm;
+        }
+    }
+}
+
+</script>
+
+
+<style>
+
+#search {
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+}
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label, h2 {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0px 5px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+
+button {
+  background: blue;
+  border: 0;
+  padding: 12px 16px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.card-area {
+display: flex;
+justify-content: center;
+}
+.card {
+background-color: rgb(252, 233, 233);
+width: 60%;
+display: flex;
+flex-direction: column ;
+align-items: flex-start;
+padding: 30px
+}
+
+
+.score {
+   display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd
+}
+.icon {
+ align-self: center;
+}
+input[type="checkbox"]
+ {
+  display: inline-block;
+  width: 16px;
+  margin:0 30px 0 0;
+  position: relative;
+  top: 2px;
+  
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+
+
+</style>
---
+++ src/components/ViewAllSets.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+       <p>{{job.name}}</p>
+      </li>
+      
+    </ul>
+   
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+    
+  data() {
+    return {
+      jobs: []
+    };
+  },
+mounted(){
+    fetch('http://localhost:9900/jobs')
+    .then(res => res.json())
+    .then(data => this.jobs = data)
+    .catch(err => console.log(err.message))
+    
+    console.log(this.jobs)
+}
+
+
+
+//   async created() {
+//     try {
+//       const res = await axios.get(`http://localhost:9900/todos`);
+
+//       this.todosArray = res.data;
+//       console.log("This" + todos)
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
+};
+</script>
+
+<style scoped>
+h1 {
+  text-decoration: underline;
+}
+
+li {
+  color: white;
+}
+</style>
